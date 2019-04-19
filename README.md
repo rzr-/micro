f688fa89237ee1d544f683c988e20b2a29536dc0
rzr- <axel.hilmi@mail.ru>
2019-04-19 18:56:22 +0300

    Fixed error in test_cases README.md added

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mathematical expression calculator
+Reverse Polish Notation + Shunting-yard algorithm implementation of basic calculator.
+
+### Usage
+**Compile and run:**
+```
+./build.sh
+```
+**Clean project makefiles:**
+```
+./build.sh clean
+```
+
+### Dependencies
+This project requires **C++17** enabled compiler mainly because of usage of structured bindings.
+
--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,16 @@ bool isEqual(double x, double y);
 double quiet_nan = std::numeric_limits<double>::quiet_NaN();
 
 const std::vector <std::tuple<std::string, double>> test_cases {
-    { "1920*720*4*2",                     double(11059200) },
+    { "1920*720*4*              2",                     double(11059200) },
     { "(64+16+24+4+16+16+24+16)/40",      double(4.5) },
     { "32 ^ 4 + (11-(3 * 2)) / 332",      double(1048576.01506024) },
     { "32 ^-4 + (11-(3 * 2)) / 332",      double(0.01506119) },
     { "32 ^ (-4) + (11-(3 * 2)) / 332",   double(0.01506119) },
     { "(32) ^ (-4) + (11-(3 * 2)) / 332", double(0.01506119) },
     { "32 ^ 4 + (11-(3 * 2)) / 2",        double(1048578.5) },
-    { "(4+3)*2-(7+3)/2",                  double(9) },
+    { "(4+3)*2-(7+3)/2",   double(9) },
+    { "(4+3.)*2-(7+3)/2",  double(9) },
+    { "(4+3.0)*2-(7+3)/2", double(9) },
     // Error cases
     { "(4+3)*2-(7+3)/0",     quiet_nan },
     { "(4+3)-*2-(7+3)/0",    quiet_nan },
@@ -22,8 +24,7 @@ const std::vector <std::tuple<std::string, double>> test_cases {
     { "((=4+3)*2-(7+3)/0",   quiet_nan },
     { "(4(+3)*2-(7+3)/0",    quiet_nan },
     { "(4(+3))*2-(7+3)/0",   quiet_nan },
-    { "(4+3.)*2-(7+3)/2",    quiet_nan },
-    { "(4+3.0)*2-(7+3)/2",   quiet_nan },
+    
     { "(4+3)*2-(7+3)/2.9",   quiet_nan },
     { "(4+3...)*2-(7+3)/2",  quiet_nan },
     { "(4+3.2.)*2-(7+3)/2",  quiet_nan },
